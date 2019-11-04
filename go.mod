5a4ce7a1d47549a8f16c9082a7301f3345507767
zengjie <zengjie@jikedata.com>
2019-11-04 12:55:17 +0800

    初始化数据库配置

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module goApiFrame
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.4.0
+require (
+	github.com/gin-gonic/gin v1.4.0
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/go-xorm/xorm v0.7.9
+	github.com/kr/pretty v0.1.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2
+	xorm.io/core v0.7.2
+)
--- web/common/initDataBase.go
+++ web/common/initDataBase.go
@@ -1,1 +1,30 @@
 package common
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+	"time"
+	"xorm.io/core"
+)
+
+var Engine *xorm.Engine
+
+func InitDataEngine() {
+	Engine, err := xorm.NewEngine(MyConfig.ConnectType, MyConfig.Connect)
+	if err != nil {
+		fmt.Println("init data engine err:", err)
+	}
+	//在控制台打印出SQL语句
+	Engine.ShowSQL(true)
+	//设置结构体到表结构的映射关系
+	Engine.SetMapper(core.GonicMapper{})
+	//空闲连接数
+	Engine.SetMaxIdleConns(200)
+	//打开连接数
+	Engine.SetMaxOpenConns(200)
+	//设置最大生命周期
+	Engine.SetConnMaxLifetime(5)
+	//设置时区
+	Engine.TZLocation, _ = time.LoadLocation("Asia/Shanghai")
+}

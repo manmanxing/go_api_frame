56614a0403b7e377e3e3272f2c62e69450e51ea2
zengjie <zengjie@jikedata.com>
2019-11-05 22:22:51 +0800

    自定义 govalidator tag

--- web/model/userInfo.go
+++ web/model/userInfo.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/asaskevich/govalidator"
+	"goApiFrame/web/util"
 	"time"
 )
 
@@ -11,9 +12,9 @@ type UserInfo struct {
 	Name        string    `xorm:"varchar(50) 'name'" json:"name" valid:"stringlength(1|50)"`
 	Password    string    `xorm:"varchar(50) 'password'" json:"password" valid:"stringlength(1|50)"`
 	Status      int       `xorm:"smallint(2) 'status'" json:"status" valid:"range(0|100)"`
-	Remark      string    `xorm:"varchar(500) 'remark'" json:"remark" valid:"stringlength(0|500)"`
-	StartTime   string    `xorm:"DATE 'start_time'" json:"start_time" valid:"data,required"`
-	EndTime     string    `xorm:"DATE 'end_time'" json:"end_time" valid:"data,required"`
+	Remark      string    `xorm:"varchar(500) 'remark'" json:"remark" valid:",optional"`
+	StartTime   string    `xorm:"DATE 'start_time'" json:"start_time" valid:"date,required"`
+	EndTime     string    `xorm:"DATE 'end_time'" json:"end_time" valid:"date,required"`
 	CreatedTime time.Time `xorm:"DATETIME created 'create_time'" json:"create_time" valid:"-"`
 	UpdatedTime time.Time `xorm:"DATETIME updated 'update_time'" json:"update_time" valid:"-"`
 }
@@ -25,8 +26,8 @@ func main() {
 		Password:    "123456",
 		Status:      1,
 		Remark:      "",
-		StartTime:   "",
-		EndTime:     "",
+		StartTime:   "2019-01-01",
+		EndTime:     "2019-01-02",
 		CreatedTime: time.Time{},
 		UpdatedTime: time.Time{},
 	}
@@ -34,8 +35,9 @@ func main() {
 	//当设置为true时，如果没有定义valid tag，则会提示错误
 	//当设置为false时，如果没有定义valid tag，不会提示错误。默认值就是false
 	govalidator.SetFieldsRequiredByDefault(true)
-	govalidator.TagMap["duck"] = func(str string) bool {
-		return str == "duck"
+	govalidator.TagMap["date"] = func(str string) bool {
+		_, err := time.Parse(util.DateFormat, str)
+		return err == nil
 	}
 	result, err := govalidator.ValidateStruct(u)
 	if err != nil {
---
+++ web/util/util.go
@@ -0,0 +1,4 @@
+package util
+
+const DateFormat = "2006-01-02"          //格式化日期
+const TimeFormat = "2006-01-02 15:04:05" //格式化时间

6b034de97e7e20d12d55fc8b872baee5d3e18c79
zengjie <zengjie@jikedata.com>
2019-11-12 12:14:31 +0800

    增加参数验证中间件

---
+++ web/middleware/validator/validator.go
@@ -0,0 +1,30 @@
+package validator
+
+import (
+	"fmt"
+	"github.com/asaskevich/govalidator"
+	"github.com/gin-gonic/gin"
+	"goApiFrame/web/util"
+	"time"
+)
+
+func Validator() gin.HandlerFunc {
+	//全局变量
+	//当设置为true时，如果没有定义valid tag，则会提示错误
+	//当设置为false时，如果没有定义valid tag，不会提示错误。默认值就是false
+	//字符串使用utf8.RuneCountInString统计长度
+	govalidator.SetFieldsRequiredByDefault(true)
+	govalidator.TagMap["date"] = func(str string) bool {
+		_, err := time.Parse(util.DateFormat, str)
+		return err == nil
+	}
+	return func(context *gin.Context) {
+		context.Next()
+	}
+}
+
+func CheckValidator(object interface{}) bool {
+	result, err := govalidator.ValidateStruct(object)
+	fmt.Println(err)
+	return result
+}
